b1283f0945f8b2bbfd7c36699969e8f488e1497b
Ilya <2312wsd>
2013-04-17 21:24:19 +0300

    Added random maze (without visualization, map shows in debug.log)

--- Assets/Scripts/MazeEditor.cs
+++ Assets/Scripts/MazeEditor.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using System.Collections;
 
-public class LabrintEditor : MonoBehaviour {
+public class MazeEditor : MonoBehaviour {
 	
 	
-	private EditorController.BrushStyle[,] map = new 
+	static EditorController.BrushStyle[,] map = new 
 					EditorController.BrushStyle [EditorController.mapWidth,EditorController.mapHeight];
-	private int[] line1,line2;
-	private int length = EditorController.mapWidth;
-	private int now=0;
-	private void AddLine(){
+	static int[] line1,line2;
+	static int length = EditorController.mapWidth;
+	static int now=0;
+	static System.IO.StreamWriter file;
+	static void AddLine(){
+		string de1= "",de2="";
 		for (int i=0; i<length;i++){
+			de1+=line1[i].ToString()+" ";
+			de2+=line2[i].ToString()+" ";
 			switch (line1[i]) {
-			case 1:
+			case -1:
 				map[i,now]=EditorController.BrushStyle.Wall;
 			break;
 			default:
@@ -20,7 +24,7 @@ public class LabrintEditor : MonoBehaviour {
 			break;
 			}
 			switch (line2[i]) {
-			case 1:
+			case -1:
 				map[i,now+1]=EditorController.BrushStyle.Wall;
 			break;
 			default:
@@ -28,23 +32,100 @@ public class LabrintEditor : MonoBehaviour {
 			break;
 			}
 		}
+		file.WriteLine(de1);
+		file.WriteLine(de2);
+	
 		now+=2;
 	}
-	void Start () {
+	
+	public static EditorController.BrushStyle[,] Randomize(){
+		
+		 file = new System.IO.StreamWriter("_.txt");
 		line1 = new int [length];
 		line2 = new int [length];
+		//Add first line
 		for (int i=0; i<length;i+=2){
-			line[i]=i;
-			if (i+1<length) line[i+1]=-10;
+			line1[i]=i;
+			line2[i]=-10;
+			if (i+1<length) line2[i+1] = -1;
+			if (i+1<length) line1[i+1]=-10;
 		}
 		for (int i=0; i<length-2;i+=2){
-			int toDel=line[i+1];
-			 if (line[i]!=line[i+1]) if (Random.value<=0.5f) line[i+1]=-1; 
+			int toDel=line1[i+2];
+			 if (line1[i]!=line1[i+2]) if (Random.value<=0.6f) line1[i+1]=-1; 
 				else {
-					for (int q = 0; q<length;q+=2) if (line[q]==toDel) line[q]=line[i];
+					for (int q = 0; q<length;q+=2) if (line1[q]==toDel) line1[q]=line1[i];
 				}
 			
 		}
+		
+		for (int i=0; i<length;i+=2){
+			int count=0;
+			for (int q = 0;q<length;q+=2) if (line1[i]==line1[q]) count++;
+			for (int q = 0;q<length;q+=2) if (line1[i]==line1[q]) if (count>1){
+				if (Random.value<0.6f) {
+					line2[q]=-1;
+					count--;
+				}
+			}
+		}
 		AddLine();
+		// Add lines
+		while (now+4<EditorController.mapHeight){
+			
+			for (int i=0; i<length;i++){
+				if (line1[i]==-1) line1[i]=-10;
+				if (i%2==0) if (line2[i]==-1) {
+					line1[i]=-10;
+					line2[i]=-10;
+				}
+			}
+			for (int i=0; i<length;i+=2){
+				if (line1[i]==-10) line1[i]=i+length*now;
+			}
+			
+			for (int i=0; i<length-2;i+=2){
+				int toDel=line1[i+2];
+				 if (line1[i]!=line1[i+2]) if (Random.value<=0.6f) line1[i+1]=-1; 
+					else {
+						for (int q = 0; q<length;q+=2) if (line1[q]==toDel) line1[q]=line1[i];
+					}
+				
+			}
+			
+			for (int i=0; i<length;i+=2){
+				int count=0;
+				for (int q = 0;q<length;q+=2) if (line1[i]==line1[q]) if (line2[q]!=-1) count++;
+				for (int q = 0;q<length;q+=2) if (line1[i]==line1[q]) if (count>1){
+					if (Random.value<0.6f) {
+						line2[q]=-1;
+						count--;
+					}
+				}
+			}
+			AddLine();
+		}
+			
+		//Add last line
+			for (int i=0; i<length-2;i+=2){
+				line2[i]=-1;
+				int toDel=line1[i+2];
+				 if (line1[i]!=line1[i+2]) {
+						line1[i+1]=-10; 
+						for (int q = 0; q<length;q+=2) if (line1[q]==toDel) line1[q]=line1[i];
+					}
+				
+			}
+			
+			AddLine();
+		
+		
+		file.Close();
+		
+		return map;
+	}
+	
+	void Start () {
+		
 	}
 }
